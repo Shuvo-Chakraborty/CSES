05ba43f11d823dc3af4a585f1a4db1b4a206c9cd
Shuvo Chakraborty <141842091+Shuvo-Chakraborty@users.noreply.github.com>
2024-03-22 11:52:04 +0600

    Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -3,44 +3,34 @@
 #define all(v) v.begin(),v.end()
 #define int long long
 using namespace std;
-
-void solve()
-{
-    int N, K;
-    cin >> N >> K;
-    int cnt = N;
-    if(K >= N-1)
-    {
-        cout << 1 << endl;
-    }
-    else cout << cnt << endl;
-}
-
-
+ 
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    int t;
-    cin>>t;
-    while(t--)
+ 
+    int n;
+    cin>>n;
+ 
+    if(n==1)cout<<1<<endl;
+    if(n<=3)cout<<"NO SOLUTION"<<endl;
+ 
+    int even=2;
+    while(even<=n)
     {
-        solve();
+        cout<<even<<" ";
+        even+=2;
     }
-
-
+ 
+    int odd=1;
+    while(odd<=n)
+    {
+        cout<<odd<<" ";
+        odd+=2;
+    }
+    cout<<endl;
     return 0;
 }
-
-//6
-//2 0
-//2 1
-//4 1
-//5 10
-//5 3
-//4 4
-//
-//
-
+ 
+ 
